5b53d8a89fb3dd8e4bb993f6a4bdc6e5c7c5af4d
Gusejnov Muslim <gusejnov1999@list.ru>
2018-07-04 21:11:42 +0300

    @update(Signup) adding gql

--- src/screens/Signup/index.js
+++ src/screens/Signup/index.js
@@ -1,17 +1,106 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import {
+  Container,
+  Input,
+  Label,
+  Item,
+  Header,
+  Content,
+  Form,
+  Button,
+} from 'native-base';
+import Screen from './ui/templates/Screen';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import styles from './style';
 
+const signUpMutation = gql`
+  mutation($name: String!, $email: String!, $password: String!){
+    signup(name: $name, email: $email, password: $password){
+      token
+    }
+  }
+`;
 
+@graphql(signUpMutation)
 class Signup extends Component {
   state = {
-    values: {},
+    values: {
+      name: '',
+      email: '',
+      password: '',
+    },
     errors: {},
     isSubmitting: false,
   };
 
+  submit = async () => {
+    if(this.state.isSubmitting) {
+      return;
+    }
+    this.setState({ isSubmitting: true });
+    const response = await this.props.mutate({
+      variables: this.state.values,
+    });
+
+    console.log(response);
+    this.setState({ isSubmitting: false });
+  };
+
+  onChangeText = (key, value) => {
+    this.setState(state => ({
+      values: {
+        ...state.values,
+        [key]: value,
+      },
+    }));
+  };
+
   render() {
+    const { values: { name, email, password } } = this.state;
+
     return (
-      <View></View>
+      <Screen title="Signup">
+        <Form>
+          <Item stackedLabel>
+            <Label>
+              { 'Username' }
+            </Label>
+            <Input
+              onChangeText={value => this.onChangeText('name', value)}
+              value={name}
+            />
+          </Item>
+          <Item stackedLabel>
+            <Label>
+              { 'Email' }
+            </Label>
+            <Input
+              onChangeText={value => this.onChangeText('email', value)}
+              value={email}
+            />
+          </Item>
+          <Item stackedLabel>
+            <Label>
+              { 'Password' }
+            </Label>
+            <Input
+              onChangeText={value => this.onChangeText('password', value)}
+              value={password}
+              secureTextEntry
+            />
+          </Item>
+          <Button
+            onPress={this.submit}
+            full
+            success>
+            <Text>
+              { 'Submit' }
+            </Text>
+          </Button>
+        </Form>
+      </Screen>
     );
   }
 }
