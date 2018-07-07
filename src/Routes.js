e967e29ed452be6fe0061c5192dba093a7a5221f
Gusejnov Muslim <gusejnov1999@list.ru>
2018-07-07 23:35:29 +0300

    @upgrade(Routes) adding products route

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,14 @@ import { Switch, Route } from 'react-router';
 
 import Signup from './screens/Signup';
 import Login from './screens/Login';
+import Products from './screens/Products';
 
 export default () => (
-    <NativeRouter>
-        <Switch>
-            <Route exact path="/" component={Signup}/>
-            <Route exact path="/login" component={Login}/>
-        </Switch>
-    </NativeRouter>
+  <NativeRouter>
+    <Switch>
+      <Route exact path="/" component={Signup} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/products" component={Products} />
+    </Switch>
+  </NativeRouter>
 );
