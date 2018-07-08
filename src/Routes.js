029830d90b97d8335e217faa1d8a98ca91fd8607
Gusejnov Muslim <gusejnov1999@list.ru>
2018-07-08 18:54:29 +0300

    @(update) Routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { NativeRouter, Link } from 'react-router-native';
+import { NativeRouter } from 'react-router-native';
 import { Switch, Route } from 'react-router';
 
 import Signup from './screens/Signup';
 import Login from './screens/Login';
 import Products from './screens/Products';
+import NewProduct from './screens/NewProduct';
+import Profile from './screens/Profile';
+import DefaultRoute from './common/DefaultRoute';
 
 export default () => (
   <NativeRouter>
     <Switch>
-      <Route exact path="/" component={Signup} />
+      <Route exact path="/" component={DefaultRoute} />
+      <Route exact path="/signup" component={Signup} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/products" component={Products} />
+      <Route exact path="/products/add" component={NewProduct} />
+      <Route exact path="/profile" component={Profile} />
     </Switch>
   </NativeRouter>
 );
