32e9870694b2b58b60ca2d4b5ed1c40661cfe849
Gusejnov Muslim <gusejnov1999@list.ru>
2018-11-21 23:12:24 +0300

    feature(Product): remove products

--- src/screens/Products/index.js
+++ src/screens/Products/index.js
@@ -1,47 +1,16 @@
 import React from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, AsyncStorage } from 'react-native';
 import {
   Icon,
   Button,
   Text,
 } from 'native-base';
-import { graphql } from 'react-apollo';
+import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
-
+import jwtDecode from 'jwt-decode';
 import Screen from 'ecommerce-client/src/ui/templates/Screen';
 import ProductCard from 'ecommerce-client/src/ui/organisms/ProductCard';
-
-const Products = ({ userId, data: { products }, loading, history }) => {
-  if (loading || !products) {
-    return null;
-  }
-
-  return (
-    <Screen title="Products">
-      <Button onPress={() => history.push('/products/add')} iconLeft light>
-        <Icon name="add" />
-        <Text>
-          { 'Create product' }
-        </Text>
-      </Button>
-      {
-        products.length !== 0 && (
-          <FlatList
-            keyExtractor={item => item.id}
-            data={products}
-            renderItem={({ item }) => (
-              <ProductCard
-                key={item.id}
-                userId={userId}
-                data={item}
-              />
-            )}
-          />
-        )
-      }
-    </Screen>
-  );
-};
+import { TOKEN_KEY } from 'ecommerce-client/src/constants';
 
 export const productsQuery = gql`
   {
@@ -57,4 +26,89 @@ export const productsQuery = gql`
   }
 `;
 
-export default graphql(productsQuery)(Products);
+export const deleteProductMutation = gql`
+  mutation($id: ID!) {
+    deleteProduct(where: {id: $id}) {
+      id
+    }
+  }
+`
+
+
+@compose(
+  graphql(productsQuery),
+  graphql(deleteProductMutation, { name: 'deleteProduct' })
+)
+class Products extends React.Component {
+  state = {
+      userId: null,
+  };
+
+  async componentDidMount() {
+    const token = await AsyncStorage.getItem(TOKEN_KEY); 
+    const { userId } = jwtDecode(token);
+    this.setState({
+      userId,
+    })
+  }
+
+  deleteProduct = async (id) => {
+    const { deleteProduct } = this.props;
+    await deleteProduct({
+      variables: {
+        id,
+      },
+      update: (store) => {
+        const data = store.readQuery({ query: productsQuery });
+        const updateProducts = data.products.filter(x => x.id !== id);
+        store.writeQuery({
+          query: productsQuery,
+          data: {
+            ...data,
+            products: updateProducts,
+          }
+        });
+      },
+    })
+  }
+
+  render() {
+    const { data: { products }, loading, history } = this.props;
+    const { userId } = this.state;
+    if (loading || !products) {
+      return null;
+    }
+
+    return (
+      <Screen title="Products">
+        <Button onPress={() => history.push('/products/add')} iconLeft light>
+          <Icon name="add" />
+          <Text>
+            { 'Create product' }
+          </Text>
+        </Button>
+        {
+          products.length !== 0 && (
+            <FlatList
+              keyExtractor={item => item.id}
+              data={products.map(x => ({
+                ...x,
+                showButtons: userId === x.seller.id,
+              }))}
+              renderItem={({ item }) => (
+                <ProductCard
+                  key={item.id}
+                  userId={userId}
+                  data={item}
+                  handleDelete={this.deleteProduct}
+                />
+              )}
+            />
+          )
+        }
+      </Screen>
+    );
+  }
+};
+
+export default Products;
