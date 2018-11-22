6df2867d2973d67b7f24e28830322fa0cf60eb55
Gusejnov Muslim <gusejnov1999@list.ru>
2018-11-22 22:06:31 +0300

    update(NewProduct): change to resuable Form

--- src/screens/NewProduct/index.js
+++ src/screens/NewProduct/index.js
@@ -1,30 +1,11 @@
 import React, { Component } from 'react';
-import {
-  Form,
-  Button,
-  Text,
-  View,
-} from 'native-base';
-import { Image } from 'react-native';
-import { ImagePicker } from 'expo';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { ReactNativeFile } from 'apollo-upload-client';
 
 import { productsQuery } from 'ecommerce-client/src/screens/Products';
-import TextField from 'ecommerce-client/src/ui/atoms/TextField';
+import Form from 'ecommerce-client/src/ui/organisms/Form';
 import Screen from 'ecommerce-client/src/ui/templates/Screen';
-import styles from './style';
-
-const defaultState = {
-  values: {
-    name: '',
-    price: '',
-    pictureUrl: null,
-  },
-  errors: {},
-  isSubmitting: false,
-};
 
 const createProductMutation = gql`
   mutation($name: String!, $price: Float!, $picture: Upload!) {
@@ -43,15 +24,8 @@ const createProductMutation = gql`
 
 @graphql(createProductMutation)
 class NewProduct extends Component {
-  state = defaultState;
-
-  submit = async () => {
-    if (this.state.isSubmitting) {
-      return;
-    }
-
-    this.setState({ isSubmitting: true });
-    const { pictureUrl, name, price } = this.state.values;
+  submit = async (values) => {
+    const { pictureUrl, name, price } = values;
     const picture = new ReactNativeFile({
       uri: pictureUrl,
       type: 'image/png',
@@ -80,64 +54,12 @@ class NewProduct extends Component {
     this.props.history.push('/products');
   };
 
-  pickImage = async () => {
-    const result = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true,
-      aspect: [4, 3],
-    });
-
-    if (!result.cancelled) {
-      this.onChangeText('pictureUrl', result.uri);
-    }
-  };
-
-  onChangeText = (key, value) => {
-    this.setState(state => ({
-      values: {
-        ...state.values,
-        [key]: value,
-      },
-    }));
-  };
-
   render() {
-    const { values: { name, pictureUrl, price } } = this.state;
-
     return (
-      <Screen title="NewProduct">
-        <Form style={styles.form}>
-          <TextField
-            value={name}
-            name="name"
-            placeholder="Name"
-            onChangeText={this.onChangeText}
-          />
-          <TextField
-            value={price}
-            name="price"
-            placeholder="Price"
-            onChangeText={this.onChangeText}
-          />
-          <View>
-            <Button onPress={this.pickImage}>
-              <Text>
-                { 'Pick an image from camera roll' }
-              </Text>
-            </Button>
-            {pictureUrl && (
-              <Image source={{ uri: pictureUrl }} style={{ width: 200, height: 200 }} />
-            )}
-          </View>
-          <Button
-            onPress={this.submit}
-            full
-            success
-          >
-            <Text>
-              { 'Add product' }
-            </Text>
-          </Button>
-        </Form>
+      <Screen title="Create Product">
+        <Form
+          submit={this.submit}
+        />
       </Screen>
     );
   }
