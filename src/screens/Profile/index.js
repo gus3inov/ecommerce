49306a639b79a3359f910cd83b894b34819e6202
Gusejnov Muslim <gusejnov1999@list.ru>
2018-12-01 01:43:29 +0300

    refactor(Profile)

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,32 +1,21 @@
 import React, { Component } from 'react';
 import { AsyncStorage } from 'react-native';
-import {
-  Input,
-  Label,
-  Item,
-  Form,
-  Button,
-  Text,
-} from 'native-base';
+import { Input, Label, Item, Form, Button, Text } from 'native-base';
 import { TOKEN_KEY } from '../../constants';
 import Screen from '../../ui/templates/Screen';
 
 class Profile extends Component {
-  componentDidMount() {
-
-  }
+  componentDidMount() {}
 
   logout = () => {
     AsyncStorage.setItem(TOKEN_KEY, '');
-  }
+  };
 
   render() {
     return (
       <Screen title="Profile">
         <Button onClick={this.logout}>
-          <Text>
-            { 'Logout' }
-          </Text>
+          <Text>{'Logout'}</Text>
         </Button>
       </Screen>
     );
