cf74841bfabb4b57dbd21078bfffa7ebd7a6ff9a
Gusejnov Muslim <gusejnov1999@list.ru>
2018-12-02 12:29:57 +0300

    update(apollo setup): adding web socket link

--- src/utils/apolloSetup.js
+++ src/utils/apolloSetup.js
@@ -1,14 +1,27 @@
-import ApolloClient, { InMemoryCache, ApolloLink } from 'apollo-client-preset';
+import ApolloClient, {
+  InMemoryCache,
+  ApolloLink,
+  split,
+} from 'apollo-client-preset';
+import { getMainDefinition } from 'apollo-utilities';
 import { setContext } from 'apollo-link-context';
 import { withClientState } from 'apollo-link-state';
 import { createUploadLink } from 'apollo-upload-client';
 import { AsyncStorage } from 'react-native';
+import { WebSocketLink } from 'apollo-link-ws';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
 import { TOKEN_KEY } from '../constants';
 
 export const uploadLink = createUploadLink({
   uri: 'http://10.0.3.2:4000/graphql',
   credentials: 'same-origin',
 });
+
+const clientSocket = new SubscriptionClient('ws://10.0.3.2:4000/graphql', {
+  reconnect: true,
+});
+
+const socketlink = new WebSocketLink(clientSocket);
 
 const stateLink = withClientState({
   cache: new InMemoryCache(),
@@ -37,9 +50,18 @@ export const authLink = setContext(async (_, { headers }) => {
   };
 });
 
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  socketlink,
+  ApolloLink.from([stateLink, authLink, uploadLink])
+);
+
 const client = new ApolloClient({
   uri: 'http://10.0.3.2:4000/graphql',
-  link: ApolloLink.from([stateLink, authLink, uploadLink]),
+  link,
   cache: new InMemoryCache(),
 });
 
